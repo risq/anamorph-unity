699e3f87a5855234ea321d9978d861b08703a0a1
risq <valentin.ledrapier@gmail.com>
2016-03-13 22:46:59 +0100

    First commit

---
+++ Assets/HudFollowObject.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HudFollowObject : MonoBehaviour {
+
+    public GameObject target;
+    public GameObject line;
+
+    RectTransform rectTransform;
+    RectTransform canvasRect;
+    Transform lineTransform;
+    Transform targetTransform;
+
+    LineRenderer lineRenderer;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasRect = GetComponentInParent<RectTransform>();
+        lineRenderer = line.GetComponent<LineRenderer>();
+        lineTransform = line.GetComponent<Transform>();
+        targetTransform = target.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        Vector2 viewportPoint = Camera.main.WorldToViewportPoint(targetTransform.position);
+        Vector2 lerpedPosition = Vector3.Lerp(rectTransform.anchorMin, viewportPoint, 0.1f);
+        rectTransform.anchorMin = lerpedPosition;
+        rectTransform.anchorMax = lerpedPosition;
+
+        Vector3[] points = new Vector3[2];
+        points[0] = lineTransform.position;
+        points[1] = targetTransform.position;
+        lineRenderer.SetPositions(points);
+
+        rectTransform.localRotation = Quaternion.Euler(70 - (140 * viewportPoint.y), -110 + (220 * viewportPoint.x), 0);
+    }
+}
---
+++ Assets/RandomGlitchEnabler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+public class RandomGlitchEnabler : MonoBehaviour {
+
+    public GlitchEffect glitchEffect;
+    public float averageActivationTime = 5;
+    public float averageDeactivationTime = 0.5f;
+    public float minIntensity = 0f;
+    public float maxIntensity = 2f;
+
+    void Start()
+    {
+        glitchEffect.enabled = false;
+        StartCoroutine(ActivationLoop());
+    }
+
+    IEnumerator ActivationLoop()
+    {
+        while (true)
+        {
+            glitchEffect.intensity = Random.Range(minIntensity, maxIntensity);
+            glitchEffect.enabled = true;
+            yield return new WaitForSeconds(Random.Range(0, averageDeactivationTime * 2));
+            glitchEffect.enabled = false;
+            yield return new WaitForSeconds(Random.Range(0, averageActivationTime * 2));
+        }
+    }
+}
