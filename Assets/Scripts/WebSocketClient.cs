03020850109326e7b4406313e1a97e1cf5aca1ed
risq <valentin.ledrapier@gmail.com>
2016-04-12 11:58:43 +0200

    Add websocket connection

---
+++ Assets/Scripts/WebSocketClient.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIO;
+using System.Collections.Generic;
+
+public class WebSocketClient : MonoBehaviour {
+
+    SocketIOComponent socket;
+    bool registered = false;
+
+    // Use this for initialization
+    void Start () {
+        GameObject go = GameObject.Find("SocketIO");
+        socket = go.GetComponent<SocketIOComponent>();
+       
+        socket.On("connect", OnSocketOpen);
+        socket.On("state", OnState);
+    }
+
+    public void OnSocketOpen(SocketIOEvent e)
+    {
+        Register();
+    }
+
+    public void OnClientRegisterStatus(SocketIOEvent e)
+    {
+        Debug.Log("OnClientRegisterStatus");
+        if (!e.data.GetField("err"))
+        {
+            OnSucessfulRegister();
+        }
+    }
+
+    private void OnSucessfulRegister()
+    {
+        Debug.Log("OnSucessfulRegister");
+        registered = true;
+    }
+
+    private void OnState(SocketIOEvent e)
+    {
+        Debug.Log("OnState");
+        Debug.Log(e.data);
+    }
+
+    private void Register()
+    {
+        if (!registered)
+        {
+            Debug.Log("Registering...");
+            Dictionary<string, string> data = new Dictionary<string, string>();
+            data["id"] = "12";
+            socket.On("client:register:status", OnClientRegisterStatus);
+            socket.Emit("client:register", new JSONObject(data));
+        }
+    }
+ 
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
