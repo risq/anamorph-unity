1a0009410864595fef9e1fdf11d2a187e1bb9318
risq <valentin.ledrapier@gmail.com>
2016-04-16 18:38:45 +0200

    Add hips, stomach & neck

--- Assets/SmoothFollowBone.cs
+++ Assets/SmoothFollowBone.cs
@@ -4,18 +4,21 @@ using System.Collections;
 public class SmoothFollowBone : MonoBehaviour {
 
     public Transform target;
+    public float speed = .1f;
+    
     private Transform tr;
     private Vector3 velocity = Vector3.zero;
-    public float smoothTime = 0.3F;
+    private Quaternion initialRotation;
 
     // Use this for initialization
     void Start () {
         tr = GetComponent<Transform>();
+        initialRotation = tr.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        tr.position = Vector3.SmoothDamp(transform.position, target.position, ref velocity, smoothTime);
-        tr.rotation = target.rotation;
+        tr.position = Vector3.Lerp(tr.position, target.position, Time.time * speed);
+        tr.rotation = Quaternion.Slerp(tr.rotation, Quaternion.Lerp(initialRotation, target.rotation, 0.5f) * initialRotation, Time.time * speed);
 	}
 }
