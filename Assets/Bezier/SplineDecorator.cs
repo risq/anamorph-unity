fccda59c048c7c7c22b0b6a59fa6168b429547d7
risq <valentin.ledrapier@gmail.com>
2016-05-26 10:04:16 +0200

    Add spline frequency modifier

--- Assets/Bezier/SplineDecorator.cs
+++ Assets/Bezier/SplineDecorator.cs
@@ -4,36 +4,63 @@ using System;
 public class SplineDecorator : MonoBehaviour {
 
 	public BezierSpline spline;
-	public int count;
 	public bool lookForward;
 	public Transform item;
 
     public Transform[] instances;
     private float stepSize;
+    private int count;
 
 	private void Awake () {
-		if (count <= 0) {
-			return;
-		}
-		if (spline.Loop || count == 1) {
-			stepSize = 1f / count;
-		}
-		else {
-			stepSize = 1f / (count - 1);
-		}
+        // CreateSpline(20);
+    }
+
+    public Transform[] CreateSpline(int objectsCount)
+    {
+        DeleteInstances();
+
+        count = objectsCount;
+
+        if (count <= 0)
+        {
+            return null;
+        }
+        if (spline.Loop || count == 1)
+        {
+            stepSize = 1f / count;
+        }
+        else
+        {
+            stepSize = 1f / (count - 1);
+        }
 
         Array.Resize(ref instances, count);
 
-        for (int i = 0; i < count; i++) {
-			instances[i] = Instantiate(item) as Transform;
-			Vector3 position = spline.GetPoint(i * stepSize);
+        for (int i = 0; i < count; i++)
+        {
+            instances[i] = Instantiate(item) as Transform;
+            Vector3 position = spline.GetPoint(i * stepSize);
             instances[i].transform.localPosition = position;
-			if (lookForward) {
+            if (lookForward)
+            {
                 instances[i].transform.LookAt(position + spline.GetDirection(i * stepSize));
-			}
+            }
             instances[i].transform.parent = transform;
-		}
-	}
+        }
+
+        return instances;
+    }
+
+    private void DeleteInstances()
+    {
+        if (count > 0)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Destroy(instances[i].gameObject);
+            }
+        }
+    }
 
     private void Update()
     {
