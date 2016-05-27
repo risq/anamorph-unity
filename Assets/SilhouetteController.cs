671a710f79b251ac6281d2c7f1b1a47bc5bb418e
risq <valentin.ledrapier@gmail.com>
2016-05-27 10:31:21 +0200

    Add activity silhouette modifiers

--- Assets/SilhouetteController.cs
+++ Assets/SilhouetteController.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class SilhouetteController : MonoBehaviour {
 
+    public Color PrivateColor;
+    public Color PublicColor;
+    public Color ProColor;
+
     // Activity
     ModelSwitcher LeftLowerArmModelSwitcher;
     ModelSwitcher LeftLowerArmPublicModelSwitcher;
@@ -28,6 +32,13 @@ public class SilhouetteController : MonoBehaviour {
     ModelSwitcher RightUpperArmProModelSwitcher;
     Material RightUpperArmMaterial;
 
+    ModelSwitcher LeftElbowModelSwitcher;
+    ModelSwitcher RightElbowModelSwitcher;
+
+    ModelSwitcher LeftBottomShoulderModelSwitcher;
+    ModelSwitcher RightBottomShoulderModelSwitcher;
+
+    // Mood
     ModelSwitcher LeftLungModelSwitcher;
     Material LeftLungMaterial;
 
@@ -36,8 +47,8 @@ public class SilhouetteController : MonoBehaviour {
 
     SplineDecorator Spline;
 
-    // Use this for initialization
     void Start () {
+        // Activity
         LeftLowerArmModelSwitcher = GameObject.Find("LeftElbow/Lower Arm Model/Inner").GetComponent<ModelSwitcher>();
         LeftLowerArmPublicModelSwitcher = GameObject.Find("LeftElbow/Lower Arm Model/Public").GetComponent<ModelSwitcher>();
         LeftLowerArmPrivateModelSwitcher = GameObject.Find("LeftElbow/Lower Arm Model/Private").GetComponent<ModelSwitcher>();
@@ -62,6 +73,13 @@ public class SilhouetteController : MonoBehaviour {
         RightUpperArmProModelSwitcher = GameObject.Find("RightUpperArm/Upper Arm Model/Pro").GetComponent<ModelSwitcher>();
         RightUpperArmMaterial = GameObject.Find("RightUpperArm/Upper Arm Model/Inner").GetComponent<Renderer>().material;
 
+        LeftElbowModelSwitcher = GameObject.Find("LeftElbow/Elbow Model/Inner").GetComponent<ModelSwitcher>();
+        RightElbowModelSwitcher = GameObject.Find("RightElbow/Elbow Model/Inner").GetComponent<ModelSwitcher>();
+
+        LeftBottomShoulderModelSwitcher = GameObject.Find("LeftShoulder/Full Shoulder/Bottom Shoulder Model/Inner").GetComponent<ModelSwitcher>();
+        RightBottomShoulderModelSwitcher = GameObject.Find("RightShoulder/Full Shoulder/Bottom Shoulder Model/Inner").GetComponent<ModelSwitcher>();
+
+        // Mood
         LeftLungModelSwitcher = GameObject.Find("Left Lung Model/Inner").GetComponent<ModelSwitcher>();
         LeftLungMaterial = GameObject.Find("Left Lung Model/Inner").GetComponent<Renderer>().material;
 
@@ -78,51 +96,78 @@ public class SilhouetteController : MonoBehaviour {
 
     void UpdateData()
     {
-        float activityFreqValue = Random.Range(0, 100);
-        float activityPublicVol = Random.Range(0, 100);
-        float activityPrivateVol = Random.Range(0, 100);
-        float activityProVol = Random.Range(0, 100);
-        Color activityFreqMix = new Color(Random.value, Random.value, Random.value);
-        float moodGlobalExpressivity = Random.Range(0, 100);
-        Color moodExpressivityMix = new Color(Random.value, Random.value, Random.value);
+        // ========== Activity ==========
 
-        LeftLowerArmModelSwitcher.currentValue = activityFreqValue;
-        LeftLowerArmPublicModelSwitcher.currentValue = Random.Range(0, 100);
-        LeftLowerArmPrivateModelSwitcher.currentValue = Random.Range(0, 100);
-        LeftLowerArmProModelSwitcher.currentValue = Random.Range(0, 100);
+        // Freq
+        float activityGlobalFreq = Random.Range(0, 100);
+        float activityPrivateFreq = Random.Range(0, 100);
+        float activityPublicFreq = Random.Range(0, 100);
+        float activityProFreq = Random.Range(0, 100);
+        Color activityFreqMix = GenerateColor(activityGlobalFreq, activityPrivateFreq, activityPublicFreq);     
+
+        LeftLowerArmModelSwitcher.currentValue = activityGlobalFreq;
+        LeftLowerArmPublicModelSwitcher.currentValue = activityPublicFreq;
+        LeftLowerArmPrivateModelSwitcher.currentValue = activityPrivateFreq;
+        LeftLowerArmProModelSwitcher.currentValue = activityProFreq;
         SetColor(LeftLowerArmMaterial, activityFreqMix);
 
-        RightLowerArmModelSwitcher.currentValue = activityFreqValue;
-        RightLowerArmPublicModelSwitcher.currentValue = Random.Range(0, 100);
-        RightLowerArmPrivateModelSwitcher.currentValue = Random.Range(0, 100);
-        LeftUpperArmProModelSwitcher.currentValue = Random.Range(0, 100);
+        RightLowerArmModelSwitcher.currentValue = activityGlobalFreq;
+        RightLowerArmPublicModelSwitcher.currentValue = activityPublicFreq;
+        RightLowerArmPrivateModelSwitcher.currentValue = activityPrivateFreq;
+        LeftUpperArmProModelSwitcher.currentValue = activityProFreq;
         SetColor(RightLowerArmMaterial, activityFreqMix);
 
-        LeftUpperArmModelSwitcher.currentValue = activityFreqValue;
-        LeftUpperArmPublicModelSwitcher.currentValue = Random.Range(0, 100);
-        LeftUpperArmPrivateModelSwitcher.currentValue = Random.Range(0, 100);
-        LeftUpperArmProModelSwitcher.currentValue = Random.Range(0, 100);
+        LeftUpperArmModelSwitcher.currentValue = activityGlobalFreq;
+        LeftUpperArmPublicModelSwitcher.currentValue = activityPublicFreq;
+        LeftUpperArmPrivateModelSwitcher.currentValue = activityPrivateFreq;
+        LeftUpperArmProModelSwitcher.currentValue = activityProFreq;
         SetColor(LeftUpperArmMaterial, activityFreqMix);
 
-        RightUpperArmModelSwitcher.currentValue = activityFreqValue;
-        RightUpperArmPublicModelSwitcher.currentValue = Random.Range(0, 100);
-        RightUpperArmPrivateModelSwitcher.currentValue = Random.Range(0, 100);
-        RightUpperArmProModelSwitcher.currentValue = Random.Range(0, 100);
+        RightUpperArmModelSwitcher.currentValue = activityGlobalFreq;
+        RightUpperArmPublicModelSwitcher.currentValue = activityPublicFreq;
+        RightUpperArmPrivateModelSwitcher.currentValue = activityPrivateFreq;
+        RightUpperArmProModelSwitcher.currentValue = activityProFreq;
         SetColor(RightUpperArmMaterial, activityFreqMix);
+
+        // Volume
+        float activityGlobalVol = Random.Range(0, 100);
+        float activityPrivateVol = Random.Range(0, 100);
+        float activityPublicVol = Random.Range(0, 100);
+        float activityProVol = Random.Range(0, 100);
+
+        SetBones(activityGlobalFreq, activityPublicVol, activityPrivateVol, activityProVol);
+
+        // Photos posts volume
+        float activityPrivatePhotoVol = Random.Range(0, 100);
+        float activityPublicPhotoVol = Random.Range(0, 100);
+        float activityProPhotoVol = Random.Range(0, 100);
+
+        LeftElbowModelSwitcher.currentValue = activityPrivatePhotoVol;
+        RightElbowModelSwitcher.currentValue = activityPrivatePhotoVol;
+
+        LeftBottomShoulderModelSwitcher.currentValue = activityPublicPhotoVol;
+        RightBottomShoulderModelSwitcher.currentValue = activityPublicPhotoVol;
+
+
+
+        // ========== Mood ==========
+
+        // Expressivity
+        float moodGlobalExpressivity = Random.Range(0, 100);
+        Color moodExpressivityMix = new Color(Random.value, Random.value, Random.value);
 
         LeftLungModelSwitcher.currentValue = moodGlobalExpressivity;
         SetColor(LeftLungMaterial, moodExpressivityMix);
 
         RightLungModelSwitcher.currentValue = moodGlobalExpressivity;
         SetColor(RightLungMaterial, moodExpressivityMix);
-
-        SetBones(activityFreqValue, activityPublicVol, activityPrivateVol, activityProVol);
     }
 
     void SetColor(Material objectMaterial, Color color)
     {
-        objectMaterial.SetColor("_FresColor", color);
-        objectMaterial.SetColor("_DiffColor", color);
+        objectMaterial.SetColor("_Color", color);
+        // objectMaterial.SetColor("_FresColor", color);
+        // objectMaterial.SetColor("_DiffColor", color);
     }
 
     void SetBones(float activityFreqValue, float publicVol, float privateVol, float proVol)
@@ -135,6 +180,17 @@ public class SilhouetteController : MonoBehaviour {
             splineBones[i].transform.FindChild("Private").localScale = new Vector3(1f + privateVol / 100f * 5f, 1, 1);
             splineBones[i].transform.FindChild("Pro").localScale = new Vector3(1f + proVol / 100f * 5f, 1, 1);
         }
+    }
+
+    Color GenerateColor(float privatePart, float publicPart, float proPart)
+    {
+        publicPart = publicPart / (publicPart + privatePart + proPart);
+        privatePart = publicPart / (publicPart + privatePart + proPart);
+        proPart = publicPart / (publicPart + privatePart + proPart);
+
+        return new Color(PrivateColor.r * publicPart + PublicColor.r * publicPart + ProColor.r * proPart,
+            PrivateColor.g * publicPart + PublicColor.g * publicPart + ProColor.g * proPart,
+            PrivateColor.b * publicPart + PublicColor.b * publicPart + ProColor.b * proPart);
     }
 
     void OnGUI()
