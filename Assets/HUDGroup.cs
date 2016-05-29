fc191175ab163db1ca93e212b23fd398723389fa
risq <valentin.ledrapier@gmail.com>
2016-05-29 17:01:01 +0200

    Update focus cameras

--- Assets/HUDGroup.cs
+++ Assets/HUDGroup.cs
@@ -4,19 +4,25 @@ using DG.Tweening;
 
 public class HUDGroup : MonoBehaviour {
 
+    public GameObject linkedFocusCamera;
+
     HUDPane[] panes;
     int panesCount;
     CanvasGroup canvasGroup;
     Tweener tweener;
+
     void Start ()
     {
         canvasGroup = GetComponent<CanvasGroup>();
         panes = GetComponentsInChildren<HUDPane>();
         panesCount = panes.Length;
+
+        linkedFocusCamera.SetActive(false);
     }
 
     public void Show()
     {
+        linkedFocusCamera.SetActive(true);
         tweener.Kill();
         tweener = canvasGroup.DOFade(1, 0.5f);
         for (int i = 0; i < panesCount; i++)
@@ -27,7 +33,7 @@ public class HUDGroup : MonoBehaviour {
 
     public void Hide()
     {
-
+        linkedFocusCamera.SetActive(false);
         tweener = canvasGroup.DOFade(0, 0.5f);
         for (int i = 0; i < panesCount; i++)
         {
