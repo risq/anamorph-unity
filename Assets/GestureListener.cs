22f313d27d41f88e464fcf4ecce4607352562e5f
risq <valentin.ledrapier@gmail.com>
2016-05-29 23:11:42 +0200

    Minor update

---
+++ Assets/GestureListener.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GestureListener : MonoBehaviour, KinectGestures.GestureListenerInterface
+{
+
+
+    public bool GestureCancelled(long userId, int userIndex, KinectGestures.Gestures gesture, KinectInterop.JointType joint)
+    {
+        return false;
+    }
+
+    public bool GestureCompleted(long userId, int userIndex, KinectGestures.Gestures gesture, KinectInterop.JointType joint, Vector3 screenPos)
+    {
+        return false;
+    }
+
+    public void GestureInProgress(long userId, int userIndex, KinectGestures.Gestures gesture, float progress, KinectInterop.JointType joint, Vector3 screenPos)
+    {
+
+    }
+
+    public void UserDetected(long userId, int userIndex)
+    {
+        
+    }
+
+    public void UserLost(long userId, int userIndex)
+    {
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
