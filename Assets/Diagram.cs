3f3ef2ee136ef4795f1549050b33ee432e02fcf1
risq <valentin.ledrapier@gmail.com>
2016-06-05 12:04:26 +0200

    Update diagram

--- Assets/Diagram.cs
+++ Assets/Diagram.cs
@@ -11,6 +11,10 @@ public class Diagram : MonoBehaviour {
     public Image publicImage;
     public Image proImage;
 
+    public Text privateText;
+    public Text publicText;
+    public Text proText;
+
     void Awake () {
         UpdateDiagram();
 	}
@@ -22,11 +26,14 @@ public class Diagram : MonoBehaviour {
 
         privateImage.fillAmount = privatePart;
         privateImage.transform.localRotation = Quaternion.Euler(Vector3.zero);
+        privateText.text = Mathf.Round(privatePart * 100) + "%";
 
         publicImage.fillAmount = publicPart;
         publicImage.transform.localRotation = Quaternion.Euler(-Vector3.forward * 360 * privatePart);
+        publicText.text = Mathf.Round(publicPart * 100) + "%";
 
         proImage.fillAmount = proPart;
         proImage.transform.localRotation = Quaternion.Euler(-Vector3.forward * 360 * (privatePart + publicPart));
+        proText.text = Mathf.Round(proPart * 100) + "%";
     }
 }
