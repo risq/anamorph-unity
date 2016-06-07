4942ba6c6e11d33f507f4bc6abeb50732ba53ae1
risq <valentin.ledrapier@gmail.com>
2016-06-07 18:37:45 +0200

    Add QR Code

---
+++ Assets/QRCodeFetcher.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class QRCodeFetcher : MonoBehaviour {
+    string url = "http://localhost:8080/qr";
+
+    IEnumerator Start()
+    {
+        // Start a download of the given URL
+        WWW www = new WWW(url);
+
+        // Wait for download to complete
+        yield return www;
+
+        // assign texture
+        RawImage rawImage = GetComponent<RawImage>();
+        rawImage.texture = www.texture;
+    }
+}
